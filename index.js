9272c59c253fdd49d7996419d7e0fdb5fb0bae78
Ben <bodoom08@gmail.com>
2020-08-20 02:46:10 +0100

    Import numbers from csv

--- index.js
+++ index.js
@@ -1,39 +1,47 @@
 const accountSid = 'AC3139ddcc927750c5bbbe20bd9128db5b';
 const authToken = '57ed870a18ebe5512b5f853ca5842c62';
 const client = require('twilio')(accountSid, authToken);
+const csv = require('csv-parser');
+const fs = require('fs');
 
-// var customer_phone_number = [
-//     '+18682916682',
-//     '+18687767473',
-//     '+18687195976',
-//     '+18683586836',
-//     '+18682905134',
-//     '+18687721966',
-//     '+18682945655',
-//     '+18682870501'
-// ];
+var customer_phone_number = [];
 
 const twilio_number = '+972524725643';
 const message_service_sid = 'MG2092d357ff40798f6e2d39fad2790b78';
 const sender_id = "Tatil Life";
 const text = "Hello, Twilio Test!";
-// customer_phone_number.forEach(customer => {
 
-// client.messages
-//     .create({
-//         body: text,
-//         from: twilio_number,
-//         to: "+79841503251"
-//     })
-//     .then(message => console.log(message.sid))
-//     .catch(err => console.log("error", err));
+fs.createReadStream('data.csv')
+    .pipe(csv())
+    .on('data', async (row) => {
+        // customer_phone_number.push('+1' + row.Phone);
+        // await setTimeout(function () {
+        //     console.log('+1' + row.Phone);
+        // }, 1000)
 
-client.calls
-    .create({
-        url: 'http://demo.twilio.com/docs/voice.xml',
-        to: '+79841503251',
-        from: twilio_number
+        let promise = new Promise((resolve, reject) => {
+            setTimeout(() => resolve('+1' + row.Phone), 1000)
+        });
+
+        let result = await promise; // wait until the promise resolves (*)
+
+        console.log(result);
     })
-    .then(call => console.log(call.sid))
-    .catch(err => console.log("error", err));
-// });
+    .on('end', () => {
+        console.log('CSV file successfully processed');
+
+        customer_phone_number.forEach(async customer => {
+
+            // client.messages
+            //     .create({
+            //         body: text,
+            //         from: twilio_number,
+            //         to: "+79841503251"
+            //     })
+            //     .then(message => console.log(message.sid))
+            //     .catch(err => console.log("error", err));
+
+        });
+
+    });
+console.log("cool");
